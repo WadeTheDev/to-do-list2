3d1798b4fc902e4a42f4b36736845298501c47be
Clara Pons <clarapons@macbook-air-de-clara.home>
2022-07-22 11:18:17 +0200

    css

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+   
   );
 }
 
