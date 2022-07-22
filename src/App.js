aa4441114d6d70bd647fb74d9125309ce8774e65
Clara Pons <clarapons@macbook-air-de-clara.home>
2022-07-22 11:19:23 +0200

    error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './index.css'
 
 function App() {
   return (
-   
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button className='px-6 h-12 uppercase font-semibold tracking-wider border-2 border-black bg-teal-400 text-black'>Click on me !</button>
+    </div>
   );
 }
 
