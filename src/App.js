0400e035a6dc58d7c6cbcc95c9769f40c0ec6a50
WadeTheDev <echo1006j@gmail.com>
2022-07-22 11:01:10 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Form from './components/Form'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      tasks: [],
+      task : ""
+    }
+  }
+
+  addTask = (e) => {
+    this.setState({task: e.target.value})
+    console.log(this.state.task)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="text-primary">Todo List</h1>
+        <Form addTask={this.addTask}/>
+      </div>
+    );
+  }
 }
 
 export default App;
