ac921df798c5ff5186e622970a5899bee3c54f38
WadeTheDev <echo1006j@gmail.com>
2022-07-22 15:40:11 +0200

    delete button

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ class App extends React.Component {
     super()
     this.state = {
       tasks: [],
+      modify: false
     }
   }
 
   addTask = (taskName) => {
-    const task = {description: taskName, status: "to do"}
+    const task = {description: taskName, status: "To do"}
     const taskClone = [task,...this.state.tasks]
     this.setState({tasks : taskClone})
   }
@@ -23,13 +24,44 @@ class App extends React.Component {
     this.setState({tasks: taskClone})
   }
 
-  render() {
+  handleChangeStatus = (e, index) => {
+    const taskClone = [...this.state.tasks]
+    taskClone[index].status = e.target.value
+    this.setState({tasks: taskClone})
     console.log(this.state.tasks)
+  }
+
+  test = (e) => {
+    console.log(e.target.value)
+  }
+
+  handleModifyTask = () => {
+    if (this.state.modify) {
+      this.setState({modify: false})
+    }else{
+      this.setState({modify: true})
+    }
+  }
+
+  handleChangeDescription = (index, e) => {
+    const taskClone = [...this.state.tasks]
+    taskClone[index].description = e.target.value
+    this.setState({tasks: taskClone})
+  }
+
+  render() {
     return (
       <div>
         <h1>Todo List</h1>
         <Form addTask={this.addTask}/>
-        <List tasks={this.state.tasks} deleteTask={this.deleteTask}/>
+        <List tasks={this.state.tasks}
+         deleteTask={this.deleteTask}
+        changeStatus={this.handleChangeStatus}
+        modify={this.state.modify}
+        modifyBox={this.handleModifyTask}
+        changeDescription={this.handleChangeDescription}
+        test={this.test}
+        />
       </div>
     );
   }
--- src/components/List.js
+++ src/components/List.js
@@ -13,11 +13,27 @@ class List extends React.Component {
             <ul>
             {this.props.tasks.map((task,i) =>{
             return (
+                <div>
+                { this.props.modify === false ? (
                     <li key={i}>
                     <h2>Tache {i}</h2>
                     <p>{task.description}</p>
+                    <input type='text'></input>
+                    <select onClick={(event) => this.props.changeStatus(event,i)}>
+                    <option value="To do">To do</option>
+                    <option value="Doing">Doing</option>
+                    <option value="Done">Done</option>
+                    </select>
+                    <button onClick={this.props.modifyBox}>Modifier</button>
                     <button onClick={() => this.props.deleteTask(i)}>Supprimer</button>
                     </li>
+                ) : 
+                (<div key={i}>
+                <input onChange={(event) => this.props.changeDescription(i,event)} type='text'></input>
+                <button onClick={this.props.modifyBox}>Valider</button>
+                </div>)
+                    }
+                </div>
                 )
                 // console.log(this.props.tasks[i])
             })}
